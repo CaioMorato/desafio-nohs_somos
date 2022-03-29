d226db094dd036d1aaabafceb667e2ba87833485
Caio Morato <morato_dev@protonmail.com>
2022-03-29 00:30:51 -0300

    :ok_hand: revision: removes print line 28

--- nohs_somos/api_project/views.py
+++ nohs_somos/api_project/views.py
@@ -25,7 +25,6 @@ def listar_salvar_autores(request):
 
         if serializer.is_valid():
             serializer.save()
-            print(serializer.data)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
